ea2eddca121679f0f219620d08be3b4a0d036511
priest-2105 <priest2105@gmail.com>
2025-02-12 17:10:41 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'QR code Generator',
+  description: '',
+  generator: '',
 }
 
 export default function RootLayout({
@@ -14,6 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/logo-favicon.png" type="image/x-icon" />
       <body>{children}</body>
     </html>
   )
